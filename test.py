206cb30ab63d8b2f46e28724c151ac3535c8e047
Evan Kaiden <67209702+Evan-Kaiden@users.noreply.github.com>
2025-03-06 17:13:38 -0800

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import soundfile as sf
-
-wav_path = "/Users/evankaiden/Documents/SimpleTransfromerTTS-copy/content/speech_dataset/LJSpeech-1.1/wavs/LJ028-0440.wav"
-
-
-import torchaudio
-
-waveform, sample_rate = torchaudio.load(wav_path)
